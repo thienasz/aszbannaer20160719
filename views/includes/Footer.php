71ef5a4dca51fc6c7c693b3c4744430e6f7bba32
pham dao <daopt22@gmail.com>
2016-08-16 18:08:37 +0700

    add font color

--- views/includes/Footer.php
+++ views/includes/Footer.php
@@ -11,5 +11,7 @@
 <script src="assets/js/script.js"></script>
 <script src="assets/js/interactcus.js"></script>
 <script src="assets/js/libs/bootstrap.min.js"></script>
+<script src="assets/js/libs/bootstrap-colorpicker.min.js"></script>
+<script src="assets/js/libs/bootstrap-colorpicker-plus.js"></script>
 </body>
 </html>
