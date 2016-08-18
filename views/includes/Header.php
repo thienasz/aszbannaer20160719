018772f9fa38c22e9a9026c11988b10d9690745f
Tuyet Nhung <nhungtuyet9525@gmail.com>
2016-08-18 17:07:33 +0700

    edit interface for banner

--- views/includes/Header.php
+++ views/includes/Header.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/jquery-ui.css">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Gochi+Hand" rel="stylesheet">
     <link href="assets/css/bootstrap-colorpicker.min.css" rel="stylesheet">
     <link href="assets/css/bootstrap-colorpicker-plus.css" rel="stylesheet">
     <script> var root = '<?php echo BASE_URL; ?>' </script>
