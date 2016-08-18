bc80861b052d4f8c0d6370c58666a583e69aaf1e
thienasz <thien.dang@nccsoft.vn>
2016-08-18 23:01:51 +0700

    add example for transparent

--- controllers/Layout.php
+++ controllers/Layout.php
@@ -36,8 +36,8 @@ class Layout extends BaseController
                 $image = imagecreatefromjpeg ($link);
             }
             imagecopyresampled($image_p, $image, 0, 0, 0, 0, $new_width, $new_height, $width, $height);
-            $first = $this->rotate_transparent_img( $image_p, 180-$el['rotate'] );
-            //$first = imagerotate($image_p,180-$el['rotate'], 0);
+//            $first = $this->rotate_transparent_img( $image_p, 180-$el['rotate'] );
+            $first = imagerotate($image_p,180-$el['rotate'], 0);
             imagecopymerge($outputImage, $first, $el['left'], $el['top'],0,0, $el['width'], $el['height'],100);
 
         }
@@ -56,5 +56,39 @@ class Layout extends BaseController
 
         return $result;
     }
+    function testRotate() {
+        $x = 808;
+        $y = 377;
+        $background = imagecreatetruecolor($x, $y);
+        $red = imagecolorallocate($background, 255, 0, 0);
+        imagefill($background,0,0,$red);
+//        imagepng($background, $background);
+        $link = "C:/AppServ/www/banner/images/backgrounds/bg5.jpg";
+        $source = imagecreatefromjpeg ($link);
+        imagealphablending($source, false);
+        imagesavealpha($source, true);
 
+        $rotation = imagerotate($source, 10, imageColorAllocateAlpha($source, 0, 0, 0, 127));
+
+        $this->imagecopymerge_alpha($background, $rotation, 0,0,0,0, $x, $y, 100); // merge with no background
+
+        header('Content-type: image/png');
+        imagepng($background, null);
+        imagedestroy($source);
+        imagedestroy($rotation);
+    }
+
+    function imagecopymerge_alpha($dst_im, $src_im, $dst_x, $dst_y, $src_x, $src_y, $src_w, $src_h, $pct){
+        // creating a cut resource
+        $cut = imagecreatetruecolor($src_w, $src_h);
+
+        // copying relevant section from background to the cut resource
+        imagecopy($cut, $dst_im, 0, 0, $dst_x, $dst_y, $src_w, $src_h);
+
+        // copying relevant section from watermark to the cut resource
+        imagecopy($cut, $src_im, 0, 0, $src_x, $src_y, $src_w, $src_h);
+
+        // insert cut resource to destination image
+        imagecopymerge($dst_im, $cut, $dst_x, $dst_y, 0, 0, $src_w, $src_h, $pct);
+    }
 }
